020c3bb1fbd927ef66c1a375675a357738e7731c
Tania Allard <taniar.allard@gmail.com>
2020-11-27 13:00:53 +0000

    :truck: Rename package for consistency

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,16 @@ import {
 import { IThemeManager } from '@jupyterlab/apputils';
 
 /**
- * A plugin for trallard/jupyter_pitaya_smoothie
+ * A plugin for trallard/jupyter-pitaya-smoothie
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: '@trallard/jupyter_pitaya_smoothie:plugin',
+  id: '@trallard/jupyter-pitaya-smoothie:plugin',
   requires: [IThemeManager],
   activate: function (app: JupyterFrontEnd, manager: IThemeManager) {
-    const style = '@trallard/jupyter_pitaya_smoothie/index.css';
+    const style = '@trallard/jupyter-pitaya-smoothie/index.css';
 
     manager.register({
-      name: 'jupyter_pitaya_smoothie',
+      name: 'Pitaya Smoothie',
       isLight: false,
       load: () => manager.loadCSS(style),
       unload: () => Promise.resolve(undefined)
